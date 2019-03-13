b4a6ed3c3689f443c1d052c9f39d47f95a680805
lidp <741386928@qq.com>
2019-03-13 21:50:23 +0800

    dubbo test

--- src/main/java/provider/Test.java
+++ src/main/java/provider/Test.java
@@ -2,6 +2,11 @@ package provider;
 
 import org.springframework.stereotype.Service;
 import service.DubboTest;
+import vo.TestReq;
+import vo.TestResp;
+
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class Test implements DubboTest {
@@ -9,4 +14,30 @@ public class Test implements DubboTest {
     public String sayHello() {
         return "hi, my name is lidp !";
     }
+
+    @Override
+    public String sayIn(String in) {
+        return in;
+    }
+
+    @Override
+    public String sayMap(Map<String, Object> map) {
+        Set<String> set = map.keySet();
+        StringBuilder sb = new StringBuilder("内容是:");
+        for (String s : set){
+            sb.append(map.get(s));
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public TestResp sayObject(TestReq req) {
+        Map<String, Object> m = req.getMap();
+        System.out.println(m.get("1"));
+        System.out.println(req.getWords());
+        TestResp resp = new TestResp();
+        resp.setAns(req.getWords());
+        return resp;
+    }
+
 }
--- src/main/java/service/DubboTest.java
+++ src/main/java/service/DubboTest.java
@@ -1,5 +1,13 @@
 package service;
 
+import vo.TestReq;
+import vo.TestResp;
+
+import java.util.Map;
+
 public interface DubboTest {
     String sayHello();
+    String sayIn(String in);
+    String sayMap(Map<String, Object> map);
+    TestResp sayObject(TestReq req);
 }
